16c324a15be9a83b55472a2567c58187f16cc363
Mateusz Poźniak <mateusz.pozniak.93@gmail.com>
2019-10-05 17:13:33 +0200

    change api get item logic

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -31,7 +31,7 @@ class UserController extends AbstractController
         return new JsonResponse(null, Response::HTTP_OK);
     }
 
-    public function getUser(Request $request, Serializer $serializer)
+    public function getUserData(Request $request, Serializer $serializer)
     {
         $repository = $this->getDoctrine()->getRepository(User::class);
         $id = $request->query->getInt('id');
