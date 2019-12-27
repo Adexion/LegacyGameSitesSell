76eb2f8e8d1181f843e6bfa926337fd15ca1f4f8
Mateusz Poźniak <mateusz.pozniak.93@gmail.com>
2019-12-27 20:30:34 +0100

    repair

--- src/Serializer/CustomSerializer.php
+++ src/Serializer/CustomSerializer.php
@@ -25,7 +25,7 @@ class CustomSerializer
     public function mergeDataWithEntity($entity, $data): array
     {
         return array_filter(
-            array_merge($data, $this->serialize($entity)->getArray())
+            array_merge($this->serialize($entity)->getArray(), $data)
         );
     }
 }
